5d92990ab8a191f0996f898e52344dd444e88fb2
bernardo campos <berposdzn@gmail.com>
2024-06-26 18:09:05 +0100

    consonants display

--- src/components/Consonants.jsx
+++ src/components/Consonants.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import styles from './consonants.module.css'
+import { hangeul } from '../HangeulData';
 
 export default function Consonants() {
+
+  const renderConsonants = () => {
+    return hangeul.consonants.slice(0, 5).map(consonant => (
+      <div key={consonant.id} className={styles.consonantItem}>
+        {consonant.char}
+      </div>
+    ));
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
         <span className={styles.english}>Consonants </span>
         <span className={styles.korean}>자음</span>
       </div>
-      {/* <div>TODO: ADD CONSONANTS </div> */}
+      <div className={styles.consonantsGrid}>
+        {renderConsonants()}
+      </div>
     </div>
   )
 }
