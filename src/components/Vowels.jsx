dd6afb35193b2c2ecbaa045a4d48557142dbf2d6
bernardo campos <berposdzn@gmail.com>
2024-06-26 20:50:13 +0100

    vowels table

--- src/components/Vowels.jsx
+++ src/components/Vowels.jsx
@@ -1,13 +1,40 @@
 import styles from './vowels.module.css';
+import { hangeul } from '../HangeulData';
+
 
 export default function Vowels() {
+
+  const groupVowels = () => {
+    const groups = [];
+    for (let i = 0; i < hangeul.vowels.length; i += 6) {
+      groups.push(hangeul.vowels.slice(i, i + 6));
+    }
+
+    return (
+      <div>
+        {groups.map((group, index) => (
+          <div key={index}>
+            {group.map(vowel => (
+              <div key={vowel.id} className={styles.vowelItem}>
+                {vowel.char}
+              </div>
+            ))}
+            {index < groups.length - 1 && <hr className={styles.line} />}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
         <span className={styles.english}>Vowels </span>
         <span className={styles.korean}>모음</span>
       </div>
-      {/* <div>TODO: ADD VOWELS</div> */}
+      <div className={styles.consonantsGrid}>
+        {groupVowels()}
+      </div>
     </div>
   )
 }
